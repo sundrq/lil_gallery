e5de591a0116471d92a4394b61c983a79f5d54ec
sundrq <sandramny000@gmail.com>
2024-02-10 13:36:27 +0100

    Popup is 50% done. Animations are added. But still needs correction.

---
+++ functions.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector("#close").addEventListener("click", function () {
+        document.querySelector(".popup").style.display = "none";
+    });
+
+// Doesn't work for a moment.
