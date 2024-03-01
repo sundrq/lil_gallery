c2f4530f263a49ac558f31b32e74980e6bc52bd6
sundrq <sandramny000@gmail.com>
2024-03-01 11:14:09 +0100

    Close button works

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
+// const popupLinks = document.querySelectorAll(.popup-link);
 
 
-let unlock = true;
+// let unlock = true;
 
-const timeout = 200;
+// const timeout = 200;
+
+// if (popupLinks.lenght > 0) {
+//     for (let index = 0; index < popupLinks.lenght ) 
+// }
