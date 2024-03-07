639aebeb56b3b6fe0fee7c8a238dc77c750b74fc
sundrq <sandramny000@gmail.com>
2024-03-07 20:02:58 +0100

    1st audio added, On (Yes) button is functional

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// var x = document.getElementById("Music1");
 
-// function playAudio() {
-//   x.play();
-// }
-
-// function pauseAudio() {
-//   x.pause();
-// }
+var audio = document.getElementById('audio_music');
+var yes_button = document.getElementById('yes_button');
+var no_button = document.getElementById('no_button');
+var count = 0;
 
 
-var gfg = document.getElementById("Music1");
-        function playAudio() {
-            gfg.play();
-        }
-        function pauseAudio() {
-            gfg.pause();
-        }
+function play(){
+  if(count == 0){
+    count = 1;
+    audio_music.play();
+  }
+}
+
+function stop(){
+  play()
+  audio_music.stop();
+  audio_music.currentTime = 0;
+
+}
