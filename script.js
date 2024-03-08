97caabefecde12fb7301cc0f6da607297ee5057c
sundrq <sandramny000@gmail.com>
2024-03-08 14:55:42 +0100

    Future audio contols popup is 1/2 designed

--- script.js
+++ script.js
@@ -4,25 +4,23 @@ var yes_button = document.getElementById('yes_button');
 var no_button = document.getElementById('no_button');
 var count = 0;
 
-
-function play(){
-  if(count == 0){
-    count = 1;
-    audio_music.play();
+  function play(){
+  
+    if(count == 0){
+      count = 1;
+      audio_music.play();
+    }
+  
   }
-}
-
-function stop(){
-
-
-  if(count == 1){
-    count = 0;
-    audio_music.pause();
+  
+  
+  function stop(){
+  
+    if(count == 1){
+      count = 0;
+      audio_music.pause();
+    }
+    audio_music.currentTime = 0;
+  
   }
 
-
-  // play()
-  // audio_music.pause();
-  audio_music.currentTime = 0;
-
-}
