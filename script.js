37b834a88a0e6ff4652e172d2776a2afadf7500b
sundrq <sandramny000@gmail.com>
2024-09-15 02:12:30 +0200

    Bugs fixed, fixed the h1 cover issue

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
+// ------ Play/Stop Buttons ------ //
 
-var audio = document.getElementById('audio_music');
-var yes_button = document.getElementById('yes_button');
-var no_button = document.getElementById('no_button');
-var count = 0;
+window.onload = function () {
+  const images = document.querySelectorAll(".IMG");
 
-  function play(){
-  
-    if(count == 0){
+  const audio = document.getElementById("audio_music");
+  const yes_button = document.getElementById("yes_button");
+  const no_button = document.getElementById("no_button");
+  let count = 0;
+
+  function play() {
+    if (count === 0) {
       count = 1;
-      audio_music.play();
+      audio.play();
     }
-  
   }
-  
-  
-  function stop(){
-  
-    if(count == 1){
+
+  function stop() {
+    if (count === 1) {
       count = 0;
-      audio_music.pause();
+      audio.pause();
     }
     audio_music.currentTime = 0;
-  
   }
 
+  yes_button.addEventListener("click", play);
+  no_button.addEventListener("click", stop);
+};
 
-document.querySelectorAll('.NatureOne .NatureTwo .theSecondSectionOne .theSecondSectionTwo img').forEach(IMG =>{
-  IMG.onclick = () =>{
-     document.querySelector('.popupIMG').style.display = 'block'; 
-     document.querySelector('.popupIMG img').src = IMG.getAttribute('src');
+// ----- Image Popup ----- //
 
-  } 
+// document.querySelectorAll('.NatureOne .NatureTwo .theSecondSectionOne .theSecondSectionTwo img').forEach(IMG =>{
+//   IMG.onclick = () =>{
+//      document.querySelector('.popupIMG').style.display = 'block';
+//      document.querySelector('.popupIMG img').src = IMG.getAttribute('src');
 
+//   }
+
+// });
+
+//   document.querySelector('.popupIMG span').onclick = () =>{
+//   document.querySelector('.popupIMG').style.display = 'none';
+//   }
+
+// ----- Cursos ----- //
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach(function (circles) {
+  circle.x = 0;
+  circle.y = 0;
 });
 
-  document.querySelector('.popupIMG span').onclick = () =>{
-  document.querySelector('.popupIMG').style.display = 'none';
-  }
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
 
-
-  
+  circles.forEach(function (circle, index) {
+    circle.style.left = coords.x + "px";
+    circle.style.top = coords.y + "px";
+  });
+});
